ab2c20fb0d8974f69ea05b79e579c86af319d904
Colin Mason <masonc789@gmail.com>
2026-09-22 16:31:57 -0400

    add attract mode trace scenario

---
+++ tools/scenarios/attract.lua
@@ -0,0 +1,7 @@
+-- Boot and one attract loop: warning, calibration, copyright, logo, title, demo, ranking.
+return {
+	length = 4200,
+	trace = { {1, 20}, {400, 20}, {700, 20}, {1100, 20}, {1400, 20}, {1800, 20},
+	          {2300, 40}, {2800, 40}, {3400, 40}, {3700, 40}, {4000, 40} },
+	snap = { 3700, 4000 },
+}
